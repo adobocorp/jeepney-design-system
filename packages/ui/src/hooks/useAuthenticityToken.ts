d285ba4df5132ab7fe44fffa9d312c62289e4dad
Nicu Listana <niculistana@gmail.com>
2025-09-02 22:45:36 -0700

    remove other files

--- packages/ui/src/hooks/useAuthenticityToken.ts
+++
@@ -1,7 +0,0 @@
-export const useAuthenticityToken = (): string => {
-  const token =
-    (document?.querySelector("meta[name=csrf-token]") as HTMLMetaElement)
-      ?.content || "";
-
-  return token;
-};
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,54 +1,11 @@
-// Sub-components (for customization)
-export { SignInFormLayout } from "./components/form/fieldset/sign-in-form-layout";
-export { SignUpFormLayout } from "./components/form/fieldset/sign-up-form-layout";
-
-// General Components
 export { Button as ButtonComponent, COLOR } from "./components/button";
 export { Carousel } from "./components/carousel";
-export { HEADING, Typography } from "./components/typography";
-
-// Hooks
-export { useAuthenticityToken } from "./hooks/useAuthenticityToken";
-export { useForgotPassword } from "./hooks/useForgotPassword";
-export { useMasked } from "./hooks/useMasked";
-export { useResetPassword } from "./hooks/useResetPassword";
-export { useSignIn } from "./hooks/useSignIn";
-export { useSignUp } from "./hooks/useSignUp";
-
-// API functions
-export { forgotPassword } from "./api/forgotPassword";
-export { resetPassword } from "./api/resetPassword";
-export { signIn } from "./api/signIn";
-export { signUp } from "./api/signUp";
-
-// Types
-export type {
-  ForgotPasswordError,
-  ForgotPasswordRequest,
-  ForgotPasswordResponse,
-} from "./api/forgotPassword";
-export type {
-  ResetPasswordError,
-  ResetPasswordRequest,
-  ResetPasswordResponse,
-} from "./api/resetPassword";
-export type { SignInError, SignInRequest, SignInResponse } from "./api/signIn";
-export type { SignUpError, SignUpRequest, SignUpResponse } from "./api/signUp";
 export type {
   CarouselContent,
   CarouselContentProps,
   CarouselProps,
 } from "./components/carousel/types";
-export type { UseForgotPasswordOptions } from "./hooks/useForgotPassword";
-export type { UseResetPasswordOptions } from "./hooks/useResetPassword";
-export type { UseSignInOptions } from "./hooks/useSignIn";
-export type { UseSignUpOptions } from "./hooks/useSignUp";
-export type {
-  ApiCallMixins,
-  ForgotPasswordFormProps,
-  FormError,
-  ResetPasswordFormProps,
-  SignInFormProps,
-  SignUpFormProps,
-  User,
-} from "./types";
+export { EmailField } from "./components/form/field/email-field";
+export { PasswordField } from "./components/form/field/password-field";
+export { TextField } from "./components/form/field/text-field";
+export { HEADING, Typography } from "./components/typography";
